8189eb6b4c376df69328e34ce1521dacb05ac715
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-08-16 16:24:22 +0200

    creation of event assignments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 const galleryDiv = ({
   preview,
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryLi = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+  <a class = "gallery__link" href = "${original}">
+  <img class = "gallery__image" src = "${preview}"
+  alt = "${description}"
+   />
+  </a></li>`;
 
+const galleryListLi = galleryItems.map((el) => galleryLi(el)).join("");
+gallery.innerHTML = galleryListLi;
+
+gallery.addEventListener("click", onClickCollectionGal);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+function onClickCollectionGal(e) {
+  e.preventDefault();
+}
 console.log(galleryItems);
